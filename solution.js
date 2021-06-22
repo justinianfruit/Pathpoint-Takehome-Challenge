5f6797d44f588da58ecc78608ed5886c8966d192
Justine <jbarry@shapesecurity.com>
2021-06-21 19:54:47 -0600

    Order of data isn't maintained still, unsure what to do

--- solution.js
+++ solution.js
@@ -1,19 +1,18 @@
 async function sortHighestScores(filePath, numScores) {
-  const data = await import(filePath).then((data) => data.default);
-  console.log(data);
+  let fileReader = new FileReader();
+  fileReader.readAsText(filePath);
+  console.log(fileReader.result);
+  //console.log(data);
   let outputData = [];
 
-  try {
-    if (numScores > data.length) throw "numScores exceeds data length";
+  // try {
+  //   if (numScores > data.length) throw "numScores exceeds data length";
 
-    let dataKeys = Object.keys(data);
-    console.log(dataKeys);
-
-    for (let i = 0; i < numScores; i++) {}
-  } catch (err) {
-    console.log(err);
-  }
+  //   for (let i = 0; i < numScores; i++) {}
+  // } catch (err) {
+  //   console.log(err);
+  // }
   return;
 }
 
-sortHighestScores("./sample.json", 3);
+sortHighestScores("./sample.txt", 3);
