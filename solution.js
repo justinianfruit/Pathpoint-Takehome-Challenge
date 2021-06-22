e69dd33b4f352e0403186431c1e2c3d0947b0f00
Justine <jbarry@shapesecurity.com>
2021-06-21 18:59:17 -0600

    Sorting keys in descending order, throwing exception on second parameter

--- solution.js
+++ solution.js
@@ -1,7 +1,19 @@
 async function sortHighestScores(filePath, numScores) {
   const data = await import(filePath);
-  let parsedDataKeys = Object.keys(data.default);
-  console.log(parsedDataKeys);
+  let outputData;
+
+  try {
+    let dataKeys = Object.keys(data.default);
+    let dataObjects = Object.values(data.default);
+
+    dataKeys.sort().reverse();
+    console.log(dataKeys);
+
+    if (numScores > dataKeys.length) throw "numScores exceeds data length";
+  } catch (err) {
+    console.log(err);
+  }
+  return;
 }
 
-sortHighestScores("./sample.json", 3);
+sortHighestScores("./sample.json", 10);
