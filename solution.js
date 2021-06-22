2b48d8ca89b0a47bed63e69d3f26afab67945c55
Justine <jbarry@shapesecurity.com>
2021-06-21 19:43:21 -0600

    Converting sample file to be more accurate

--- solution.js
+++ solution.js
@@ -1,11 +1,12 @@
 async function sortHighestScores(filePath, numScores) {
-  const data = await import(filePath);
+  const data = await import(filePath).then((data) => data.default);
+  console.log(data);
   let outputData = [];
 
   try {
-    if (numScores > data.default.length) throw "numScores exceeds data length";
+    if (numScores > data.length) throw "numScores exceeds data length";
 
-    let dataKeys = Object.keys(data.default);
+    let dataKeys = Object.keys(data);
     console.log(dataKeys);
 
     for (let i = 0; i < numScores; i++) {}
