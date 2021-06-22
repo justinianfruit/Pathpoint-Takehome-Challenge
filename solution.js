0584dc5468971a33b40751ac5ebdea0eb62fa878
Justine <jbarry@shapesecurity.com>
2021-06-21 19:32:48 -0600

    Reverting some work, order of data wasn't preserved

--- solution.js
+++ solution.js
@@ -1,25 +1,18 @@
 async function sortHighestScores(filePath, numScores) {
   const data = await import(filePath);
-  const parsedData = Object.entries(data.default);
   let outputData = [];
 
   try {
-    if (numScores > parsedData.length) throw "numScores exceeds data length";
+    if (numScores > data.default.length) throw "numScores exceeds data length";
 
-    let scores = [];
-    for (let i = 0; i < numScores; i++) {
-      try {
-        JSON.parse(JSON.stringify(parsedData[i][1]));
-      } catch (err) {
-        throw "Invalid JSON format";
-      }
-      if (!parsedData[i][1].id) throw "No valid ID found on object";
-      outputData.push({ score: parsedData[i][0], id: "" });
-    }
+    let dataKeys = Object.keys(data.default);
+    console.log(dataKeys);
+
+    for (let i = 0; i < numScores; i++) {}
   } catch (err) {
     console.log(err);
   }
   return;
 }
 
-sortHighestScores("./sample.json", 5);
+sortHighestScores("./sample.json", 3);
